7a6a130ca150137cda722eea651744bb31542042
Furong Ye <furong1993@gmail.com>
2020-09-09 11:57:09 +0200

    first commit

---
+++ CMakeFiles/bpsolver.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/fye/Codes/Integrality-Gap-for-Bin-Packing/src/helper.cpp" "/Users/fye/Codes/Integrality-Gap-for-Bin-Packing/CMakeFiles/bpsolver.dir/src/helper.cpp.o"
+  "/Users/fye/Codes/Integrality-Gap-for-Bin-Packing/src/simpleipsolver.cpp" "/Users/fye/Codes/Integrality-Gap-for-Bin-Packing/CMakeFiles/bpsolver.dir/src/simpleipsolver.cpp.o"
+  "/Users/fye/Codes/Integrality-Gap-for-Bin-Packing/src/simplelpsolver.cpp" "/Users/fye/Codes/Integrality-Gap-for-Bin-Packing/CMakeFiles/bpsolver.dir/src/simplelpsolver.cpp.o"
+  "/Users/fye/Codes/Integrality-Gap-for-Bin-Packing/src/solution.cpp" "/Users/fye/Codes/Integrality-Gap-for-Bin-Packing/CMakeFiles/bpsolver.dir/src/solution.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Library/gurobi903/mac64/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/bpsolver.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bpsolver.dir/src/helper.cpp.o"
+  "CMakeFiles/bpsolver.dir/src/simpleipsolver.cpp.o"
+  "CMakeFiles/bpsolver.dir/src/simplelpsolver.cpp.o"
+  "CMakeFiles/bpsolver.dir/src/solution.cpp.o"
+  "libbpsolver.a"
+  "libbpsolver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bpsolver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
